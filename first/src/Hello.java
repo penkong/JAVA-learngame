76758e7b72bb026317654dabf2168bd4c999ff5e
mazdak <nazemi.works@gmail.com>
2019-06-24 05:42:09 -0700

    java game start

---
+++ first/src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello mazdak");
+
+    int myFirstNumber = 4 + 10;
+    int mySecondNumber = 6 + 10;
+    int myThirdNumber = 8 + 10;
+
+    int myTotalNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+
+    int myLsOne = 1000 - myTotalNumber;
+
+    System.out.println(myLsOne);
+
+  }
+}
