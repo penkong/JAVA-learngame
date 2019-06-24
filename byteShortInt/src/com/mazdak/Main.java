2808b400288c8aff0e360ed143399a97db1f6974
mazdak <nazemi.works@gmail.com>
2019-06-24 06:06:57 -0700

    whole numbers int byte short long

--- byteShortInt/src/com/mazdak/Main.java
+++ byteShortInt/src/com/mazdak/Main.java
@@ -3,6 +3,22 @@ package com.mazdak;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // int has width of 32;
+      int myValue = 2_253_355;
+
+      // more efficient to use get less memory %25 of integer
+      byte myByteVal = -128;
+      // java normally change expression to int must explicitly define it
+      byte myHalf = (byte) (myByteVal/2);
+
+
+      // twice amount of byte , half of integer
+      short myShortValue = 32767;
+      short myHalfShort = (short) (myShortValue/2);
+      
+      // long has width of 64; 2^63
+      long myLongVal = 100L;
+      long totalLong = 6000L + 2022L*(myShortValue + myHalfShort);
+      System.out.println("totalLong = " + totalLong);
     }
 }
